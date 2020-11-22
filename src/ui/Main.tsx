b52325d3992bac694e0b6a15ee74fe506bcd4631
Patrick Hund <pahund@team.mobile.de>
2020-11-22 17:05:37 +0100

    …squash me…
    Change-Id: I91bc287353ddda924b6de4de7dacb065e14fb0c6

--- src/ui/Main.tsx
+++ src/ui/Main.tsx
@@ -7,16 +7,7 @@ type Props = {
 
 export default function Main({ children }: Props) {
   return (
-    <main
-      className={cx(
-        "p-4",
-        "sm:p-8",
-        "pb-14",
-        "mx-auto",
-        "max-w-3xl",
-        "lg:max-w-6xl"
-      )}
-    >
+    <main className={cx("p-4", "sm:p-8", "pb-14", "mx-auto", "max-w-3xl")}>
       {children}
     </main>
   );
